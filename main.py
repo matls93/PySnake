543a7efb6713634196e7345ab7b2ac030a4c626b
Mathew <129796277+matls93@users.noreply.github.com>
2024-07-02 14:25:12 +0100

    Update main.py
    Clean up code

--- main.py
+++ main.py
@@ -6,36 +6,30 @@ from scoreboard import ScoreBoard
 from game_over import GameOver
 from random import randint
 
-
+# Set up the screen
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
+# Create game objects
 snake = Snake()
 food = Food()
 score = ScoreBoard()
 game_over = GameOver()
 
+# Initialise game state variables
 sleep_time = 0.1
 invisible = False
 slow = False
 mirror = False
 game_paused = False
 game_is_on = True
-game_quit = False
-
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
 
 
 def restart_game():
     global game_is_on, sleep_time, invisible, slow, mirror, game_paused
-    game_is_on = True
     game_paused = False
     sleep_time = 0.1
     invisible = False
@@ -43,20 +37,15 @@ def restart_game():
     mirror = False
     score.reset_scoreboard()
     snake.reset()
-    game_over.clear()
+    reset_food()
     screen.update()
-    food.move_location()
 
 
 def quit_game():
-    global game_is_on, game_quit
-    game_is_on = False
-    game_quit = True
     screen.bye()
 
 
 def handle_click(x, y):
-    global game_paused
     action = game_over.check_click(x, y)
     if action == "restart":
         restart_game()
@@ -64,7 +53,7 @@ def handle_click(x, y):
         quit_game()
 
 
-def set_food():
+def reset_food():
     global slow, mirror
     random_num = randint(0, 6)
     if random_num == 1:
@@ -75,77 +64,68 @@ def set_food():
         food.color('red')
     else:
         food.color('blue')
-
     food.move_location()
 
 
+def food_collision():
+    global sleep_time, invisible, mirror, slow
+    if slow:
+        sleep_time = 0.2
+        invisible = False
+        slow = False
+    elif mirror:
+        invisible = True
+        sleep_time = 0.1
+        mirror = False
+    else:
+        sleep_time = 0.1
+        invisible = False
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 screen.onscreenclick(handle_click)
 
 
 while game_is_on:
 
-    if not game_paused and not game_quit:
-        screen.update()
-        time.sleep(sleep_time)
-        snake.move()
+    screen.update()
+    time.sleep(sleep_time)
+    snake.move()
 
-        # detect collision with food
-        if snake.head.distance(food) < 15:
-            snake.extend()
-            score.increase_score()
-            if slow:
-                sleep_time = 0.2
-                invisible = False
-                mirror = False
-                slow = False
-            elif mirror:
-                sleep_time = 0.1
-                invisible = True
-                mirror = False
-            else:
-                sleep_time = 0.1
-                invisible = False
-                mirror = False
+    # detect collision with food
+    if snake.head.distance(food) < 17:
+        snake.extend()
+        score.increase_score()
+        food_collision()
+        reset_food()
 
-            set_food()
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        if invisible:
+            if snake.head.xcor() > 280 or snake.head.xcor() < -280:
+                snake.teleport_x()
+            elif snake.head.ycor() > 280 or snake.head.ycor() < -280:
+                snake.teleport_y()
+        else:
+            game_paused = True
+            while game_paused:
+                game_over.display()
+                screen.update()
+                time.sleep(0.3)
+            game_over.clear()
 
-        # detect collision with wall
-        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-            if invisible:
-                if snake.head.xcor() > 280 or snake.head.xcor() < -280:
-                    snake.teleport_x()
-                elif snake.head.ycor() > 280 or snake.head.ycor() < -280:
-                    snake.teleport_y()
-            else:
-                game_paused = True
-                while game_paused:
-                    if game_quit:
-                        break
-                    game_over.game_over()
-                    screen.update()
-                    time.sleep(0.3)
-                if not game_is_on:
-                    break
-                score.reset_scoreboard()
-                snake.reset()
-                game_over.clear()
-
-        # detect collision with tail
-        for segment in snake.segments[1:]:
-            if snake.head.distance(segment) < 10 and not invisible:
-                game_paused = True
-                while game_paused:
-                    if game_quit:
-                        break
-                    game_over.game_over()
-                    screen.update()
-                    time.sleep(0.3)
-                if not game_is_on:
-                    break
-                score.reset_scoreboard()
-                snake.reset()
-                game_over.clear()
-
-    screen.onscreenclick(handle_click)
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10 and not invisible:
+            game_paused = True
+            while game_paused:
+                game_over.display()
+                screen.update()
+                time.sleep(0.3)
+            game_over.clear()
 
 screen.exitonclick()
